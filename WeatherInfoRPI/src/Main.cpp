1ad8dace674811228589118e703a43d8ce3ddc66
flavien <flavien.moullec@laposte.net>
2015-01-27 22:29:54 +0100

    create WeatherInfoRPI

---
+++ WeatherInfoRPI/src/Main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(int argc, char** argv)
+{
+    cout << "Test 1 2 3" <<endl;
+
+    // TODO : start the HTTP server
+    // TODO : collect info from the sensors
+    // ...
+
+    return 0;
+}
